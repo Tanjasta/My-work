9ce71ede27aaa7ecd0a979af24803192984c615e
Tatjana <g00425678@atu.ie>
2023-05-18 17:05:18 +0200

    comments added

--- benchmarkingapp2.py
+++ benchmarkingapp2.py
@@ -1,99 +1,99 @@
-import random  # Import the random module for generating random numbers
-import time  # Import the time module for measuring the execution time
-import matplotlib.pyplot as plt  # Import the matplotlib.pyplot module for creating plots
-import pandas as pd  # Import the pandas module for data manipulation and analysis
+import random  # Imports the random module for generating random numbers
+import time  # Imports the time module for measuring the execution time
+import matplotlib.pyplot as plt  # Imports the matplotlib.pyplot module for creating plots
+import pandas as pd  # Imports the pandas module for data manipulation and analysis
 
 def bubblesort(arr):
-    n = len(arr)  # Get the length of the array
-    for i in range(n):  # Iterate over each element in the array
-        for j in range(0, n-i-1):  # Iterate from the first element to the (n-i-1)-th element
-            if arr[j] > arr[j+1]:  # Check if the current element is greater than the next element
-                # Swap the elements if they are in the wrong order
+    n = len(arr)  # Gets the length of the array
+    for i in range(n):  # Iterates over each element in the array
+        for j in range(0, n-i-1):  # Iterates from the first element to the (n-i-1)-th element
+            if arr[j] > arr[j+1]:  # Checks if the current element is greater than the next element
+                # Swaps the elements if they are in the wrong order
                 arr[j], arr[j+1] = arr[j+1], arr[j]
 
 def selectionsort(arr):
-    n = len(arr)  # Get the length of the array
-    for i in range(n):  # Iterate over each element in the array
-        min_idx = i  # Assume the current index has the minimum element
+    n = len(arr)  # Gets the length of the array
+    for i in range(n):  # Iterates over each element in the array
+        min_idx = i  # Assumes the current index has the minimum element
         
-        # Find the index of the minimum element in the unsorted portion of the array
+        # Finds the index of the minimum element in the unsorted portion of the array
         for j in range(i+1, n):
-            if arr[j] < arr[min_idx]:  # Compare the current element with the assumed minimum element
-                min_idx = j  # Update the index of the minimum element
+            if arr[j] < arr[min_idx]:  # Compares the current element with the assumed minimum element
+                min_idx = j  # Updates the index of the minimum element
         
-        # Swap the current element with the minimum element
+        # Swaps the current element with the minimum element
         arr[i], arr[min_idx] = arr[min_idx], arr[i]
         
 
 def insertionsort(arr):
-    for i in range(1, len(arr)):  # Iterate over each element in the array starting from the second element
-        key = arr[i]  # Store the current element as the key
-        j = i - 1  # Set the initial index of the previous element
+    for i in range(1, len(arr)):  # Iterates over each element in the array starting from the second element
+        key = arr[i]  # Stores the current element as the key
+        j = i - 1  # Sets the initial index of the previous element
 
-        # Move elements of arr[0...i-1], that are greater than key, to one position ahead of their current position
+        # Moves elements of arr[0...i-1], that are greater than key, to one position ahead of their current position
         while j >= 0 and arr[j] > key:
-            arr[j + 1] = arr[j]  # Shift elements to the right
-            j -= 1  # Decrement the index
+            arr[j + 1] = arr[j]  # Shifts elements to the right
+            j -= 1  # Decrements the index
 
-        arr[j + 1] = key  # Place the key at its correct position in the sorted subarray
+        arr[j + 1] = key  # Places the key at its correct position in the sorted subarray
 
 def countingsort(array, max_val):
-    m = max_val + 1  # Determine the size of the counting array
+    m = max_val + 1  # Determines the size of the counting array
 
-    count = [0] * m  # Create a counting array initialized with zeros
+    count = [0] * m  # Creates a counting array initialized with zeros
 
     for a in array:
-        # Count occurrences of each element in array
-        count[a] += 1  # Increment the count for the current element
+        # Counts occurrences of each element in array
+        count[a] += 1  # Increments the count for the current element
 
-    i = 0  # Initialize the index for the sorted array
+    i = 0  # Initialises the index for the sorted array
 
-    for a in range(m):  # Iterate over the counting array
-        for c in range(count[a]):  # Repeat for the count of each element in the counting array
-            array[i] = a  # Assign the element to the sorted array
-            i += 1  # Increment the index of the sorted array
+    for a in range(m):  # Iterates over the counting array
+        for c in range(count[a]):  # Repeats for the count of each element in the counting array
+            array[i] = a  # Assigns the element to the sorted array
+            i += 1  # Increments the index of the sorted array
 
     
 def mergesort(array):
-    print("Splitting ", array)  # Print a message indicating the current split operation
+    print("Splitting ", array)  # Prints a message indicating the current split operation
 
-    if len(array) > 1:  # Check if there is more than one element in the list
-        mid = len(array) // 2  # Calculate the middle index
-        lefthalf = array[:mid]  # Divide the list into left half
-        righthalf = array[mid:]  # Divide the list into right half
+    if len(array) > 1:  # Checks if there is more than one element in the list
+        mid = len(array) // 2  # Calculates the middle index
+        lefthalf = array[:mid]  # Divides the list into left half
+        righthalf = array[mid:]  # Divides the list into right half
 
-        mergesort(lefthalf)  # Recursively call mergeSort on the left half
-        mergesort(righthalf)  # Recursively call mergeSort on the right half
+        mergesort(lefthalf)  # Recursively calls mergeSort on the left half
+        mergesort(righthalf)  # Recursively calls mergeSort on the right half
 
         i = 0  # Index for the left half
         j = 0  # Index for the right half
         k = 0  # Index for the original list
 
         while i < len(lefthalf) and j < len(righthalf):
-            # Compare the elements from the left and right halves
+            # Compares the elements from the left and right halves
             if lefthalf[i] <= righthalf[j]:
-                array[k] = lefthalf[i]  # Place the smaller element into the original list
+                array[k] = lefthalf[i]  # Places the smaller element into the original list
                 i += 1
             else:
-                array[k] = righthalf[j]  # Place the smaller element into the original list
+                array[k] = righthalf[j]  # Places the smaller element into the original list
                 j += 1
             k += 1
 
         while i < len(lefthalf):
-            # Copy any remaining elements from the left half to the original list
+            # Copies any remaining elements from the left half to the original list
             array[k] = lefthalf[i]
             i += 1
             k += 1
 
         while j < len(righthalf):
-            # Copy any remaining elements from the right half to the original list
+            # Copies any remaining elements from the right half to the original list
             array[k] = righthalf[j]
             j += 1
             k += 1
 
 
 def generate_random_array(n):
-    return [random.randint(0, 1000) for _ in range(n)]  # Generate a list of random integers between 0 and 1000
+    return [random.randint(0, 1000) for _ in range(n)]  # Generates a list of random integers between 0 and 1000
 
 
 # Sorting algorithms
@@ -108,33 +108,33 @@ def benchmark_sorting_algorithms():
     }
 
     input_sizes = [100, 250, 500, 750, 1000, 1250, 2500, 3750, 5000, 6250, 7500, 8750, 10000]  # List of input sizes
-    results = {algorithm: [] for algorithm in algorithms}  # Create an empty dictionary to store the results
+    results = {algorithm: [] for algorithm in algorithms}  # Creates an empty dictionary to store the results
     output_results = []  # To store the results in the desired format
 
     for size in input_sizes:
         for algorithm in algorithms:
-            arr = generate_random_array(size)  # Generate a random array of the given size
-            start_time = time.time()  # Record the start time
+            arr = generate_random_array(size)  # Generates a random array of the given size
+            start_time = time.time()  # Records the start time
             if algorithm == "Counting Sort":
-                max_val = max(arr)  # Find the maximum value in the array
-                algorithms[algorithm](arr, max_val)  # Call the counting sort algorithm with the maximum value
+                max_val = max(arr)  # Finds the maximum value in the array
+                algorithms[algorithm](arr, max_val)  # Calls the counting sort algorithm with the maximum value
             else:
-                algorithms[algorithm](arr)  # Call the sorting algorithm
-            end_time = time.time()  # Record the end time
-            total_time = (end_time - start_time) * 1000  # Calculate the total execution time in milliseconds
-            avg_time = total_time / 10.0  # Calculate the average execution time over 10 iterations
-            results[algorithm].append(avg_time)  # Add the average time to the results dictionary
+                algorithms[algorithm](arr)  # Calls the sorting algorithm
+            end_time = time.time()  # Records the end time
+            total_time = (end_time - start_time) * 1000  # Calculates the total execution time in milliseconds
+            avg_time = total_time / 10.0  # Calculates the average execution time over 10 iterations
+            results[algorithm].append(avg_time)  # Adds the average time to the results dictionary
 
-    # Store the results in the desired format
+    # Stores the results in the desired format
     for algorithm in algorithms:
         output_results.append([f"{algorithm}"] + [f"{time:.3f}" for time in results[algorithm]])
 
-    # Print the results
+    # Prints the results
     columns = ["Algorithm"] + [str(size) for size in input_sizes]
     df = pd.DataFrame(output_results, columns=columns)
     print(df.to_string(index=False))  # Print the DataFrame without the index
 
-    # Plot the results
+    # Plots the results
     fig, ax = plt.subplots()
     ax.set_title("Sorting Algorithms Benchmarking")
     ax.set_xlabel("Input Size (n)")
@@ -143,9 +143,9 @@ def benchmark_sorting_algorithms():
     for algorithm in algorithms:
         ax.plot(input_sizes, results[algorithm], label=algorithm)  # Plot the running time for each algorithm
 
-    ax.legend()  # Show the legend
-    plt.savefig('sorting_algorithms.png')  # Save the plot to a file
-    plt.show()  # Display the plot
+    ax.legend()  # Shows the legend
+    plt.savefig('sorting_algorithms.png')  # Saves the plot to a file
+    plt.show()  # Displays the plot
 
 
-benchmark_sorting_algorithms()  # Run the benchmarking function
+benchmark_sorting_algorithms()  # Runs the benchmarking function
